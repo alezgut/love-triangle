f1163e43c3f66f0b659269709e1fd06091022de1
Aliaksei Ryzhankou <alezgut@gmail.com>
2018-09-17 14:06:50 +0300

    2d task

--- src/index.js
+++ src/index.js
@@ -4,4 +4,12 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var trio = 0;
+  for(var i = 0; i<preferences.length; i++){
+    if(preferences[preferences[preferences[i]-1]-1]-1==i) trio ++;
+    if(preferences[i]-1 == i) trio --;
+  }
+  console.log(trio);
+  return trio/3;
+
 };
